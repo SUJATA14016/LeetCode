da8583f381830c284e17a3a3e7c539cd82419318
vyas1416 <71184481+vyas1416@users.noreply.github.com>
2021-02-06 21:02:35 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (61.39%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* t=head;
+         ListNode* t=head;
         int count=0;
         while(t!=NULL){
             count++;
