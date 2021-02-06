55148a0f79771467601929ac119099d6551d5d26
vyas1416 <71184481+vyas1416@users.noreply.github.com>
2021-02-06 15:38:54 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.63%) - LeetHub

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int reverse(int x) {
-        int y;
+        
       long rev=0;
         while(x!=0)
         {
-            y=x%10;
-            rev=rev*10+y;
+           
+            rev=rev*10+(x%10);
             x=x/10;
             
         }
